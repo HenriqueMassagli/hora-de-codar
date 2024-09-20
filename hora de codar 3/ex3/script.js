2670257a8d1fbfa697f5c7938727f2f7ff5ce168
Henrique <henriquemassaglicavalcanti@gmail.com>
2024-09-20 14:29:43 -0300

    text

--- hora de codar 3/ex3/script.js
+++ hora de codar 3/ex3/script.js
@@ -1,3 +1,5 @@
+//3 - Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente. Exemplo: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+
 var i = 10;
 var interval = setInterval(function () {
   document.getElementById("contagem").innerText = i;
--- hora de codar 3/ex5/script.js
+++ hora de codar 3/ex5/script.js
@@ -1,3 +1,5 @@
+//5 - Faça um algoritmo que calcule e escreva a média aritmética dos dois números inteiros informados pelo usuário e todos os números inteiros entre eles. Considere que o primeiro sempre será menor que o segundo.
+
 var primeiro_numero = parseInt(prompt("Informe o primeiro número inteiro:"));
 var segundo_numero = parseInt(prompt("Informe o segundo número inteiro:"));
 
